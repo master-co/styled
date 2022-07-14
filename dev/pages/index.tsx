7fb4d98575023f6ddad1ab6155083be18070dde9
aron-tw <aron886tw@gmail.com>
2022-07-14 18:35:35 +0800

    env: next dev

--- dev/pages/index.tsx
+++ dev/pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
+import { useRef } from 'react';
 import el from '../../dist'
 
-const Button = el.button`inline-flex center-content font:14 font:semibold font:white bg:indigo px:18 h:40 r:4`;
+const BasicButton = el.button`inline-flex center-content font:14 font:semibold font:white bg:indigo px:18 h:40 r:4`;
 
 const Home: NextPage = () => {
+    const basicButtonRef = useRef<any>()
     return (
-        <>
-            <Button>Basic Button</Button>
-        </>
+        <div className="max-w:600 mx:auto p:50">
+            <BasicButton ref={basicButtonRef}>Basic Button</BasicButton>
+            {basicButtonRef?.current?.className}
+        </div>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,25 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import clsx from 'clsx'
 
 type baseType = string | number | Record<string, boolean>
 const el: {
     [key in keyof JSX.IntrinsicElements]: (defaultClassNames: TemplateStringsArray) => React.FunctionComponent<Omit<JSX.IntrinsicElements[key], 'className'> & { className?: baseType | Array<baseType> }>
-} = new Proxy(
-    {} as any,
-    {
-        get: function (target, tag: string) {
-            if (!(tag in target)) {
-                target[tag] = createElement(tag)
-            }
-
-            return target[tag]
+} = new Proxy({} as any, {
+    get: function (target, tag: string) {
+        if (!(tag in target)) {
+            target[tag] = createElement(tag)
         }
+        return target[tag]
     }
+}
 )
 
 function createElement(Tag: string) {
     return (defaultClassNames: TemplateStringsArray) => {
-        function component(props: any) {
-            return <Tag {...props} className={clsx(defaultClassNames, props.className)} />
-        }
+        const component = forwardRef((props: any, ref) => {
+            return <Tag {...props} ref={ref} className={clsx(defaultClassNames, props.className)} />
+        })
         component.displayName = Tag
-
         return component
     }
 }
