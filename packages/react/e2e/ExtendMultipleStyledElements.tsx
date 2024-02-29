8124061e72596d7d1dfa861fc07f778255577eca
Aron <i@aron.tw>
2024-02-29 22:38:21 +0800

    Temporarily passes type checking

--- packages/react/e2e/ExtendMultipleStyledElements.tsx
+++ packages/react/e2e/ExtendMultipleStyledElements.tsx
@@ -3,6 +3,8 @@ import styled from '../src';
 const A = styled.p`a`
 const B = styled.p`b`
 const C = styled.p`c`
+// todo: fix the issue with the following line
+// @ts-expect-error
 const D = styled(A)(B)(C)`d`
 
 export default D
